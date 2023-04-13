5f519da63d753b9ad2fd87bd6a5401f5a215e1a3
Monge Naoki <tgtallles362@gmail.com>
2023-04-13 18:49:25 -0300

    novos codigos e fotos aprimorações etc

--- cursoAlgoritmo/src/DecimoQuarto.java
+++ cursoAlgoritmo/src/DecimoQuarto.java
@@ -11,75 +11,51 @@ public class DecimoQuarto {
         int menor = 0;
         int s = 0;
 
+        for (cont = 1; cont <= 5; cont++) {
 
-        for(cont =1;cont<=5;cont++){
-
-
-
-            System.out.println("Digite o " +cont +"o.valor");
+            System.out.println("Digite o " + cont + "o.valor");
             n = input.nextInt();
 
-            if (n>maior) {
+            if (n > maior) {
 
                 maior = n;
             }
-            s = (s+n);
+            s = (s + n);
         }
-        System.out.println("A soma de todos os valores foi " +s);
-        System.out.println("O maior valor digitado foi: " +maior);
-        System.out.println("O menor valor digitado foi: " +menor);
-
-        }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+        System.out.println("A soma de todos os valores foi " + s);
+        System.out.println("O maior valor digitado foi: " + maior);
+        System.out.println("O menor valor digitado foi: " + menor);
 
     }
 
-
-
-
-
-
-/* //em fase de aprendizagem sobre for 
-O "for" é uma estrutura de controle em Java que permite executar um bloco de código repetidamente com base em um contador. Ele tem a seguinte sintaxe geral:
-
-for (inicialização; condição; atualização) {
-    // bloco de código a ser executado
 }
 
-"inicialização" é a expressão  é a expressão que define o valor inicial do contador. (que define o número inicial do contador.)
+// incompleto "menor"
 
-"condição" é a expressão que verifica se o loop deve continuar executando ou não. Enquanto essa condição for verdadeira, o loop continuará a ser executado. Se for falsa, o loop será encerrado. (até que número deve ir)
-
-"atualização" é a expressão que atualiza o valor do contador em cada iteração do loop. (se é pra somar ou diminuir)
-
-Aqui está um exemplo simples de um loop "for" que imprime números de 1 a 5:
-
-
-for (int i = 1; i <= 5; i++) {
-    System.out.println(i);
-}
-*/
+/*
+ * //em fase de aprendizagem sobre for
+ * O "for" é uma estrutura de controle em Java que permite executar um bloco de
+ * código repetidamente com base em um contador. Ele tem a seguinte sintaxe
+ * geral:
+ * 
+ * for (inicialização; condição; atualização) {
+ * // bloco de código a ser executado
+ * }
+ * 
+ * "inicialização" é a expressão é a expressão que define o valor inicial do
+ * contador. (que define o número inicial do contador.)
+ * 
+ * "condição" é a expressão que verifica se o loop deve continuar executando ou
+ * não. Enquanto essa condição for verdadeira, o loop continuará a ser
+ * executado. Se for falsa, o loop será encerrado. (até que número deve ir)
+ * 
+ * "atualização" é a expressão que atualiza o valor do contador em cada iteração
+ * do loop. (se é pra somar ou diminuir)
+ * 
+ * Aqui está um exemplo simples de um loop "for" que imprime números de 1 a 5:
+ * 
+ * 
+ * for (int i = 1; i <= 5; i++) {
+ * System.out.println(i);
+ * }
+ */
--- cursoAlgoritmo/src/DecimoSexto.java
+++ cursoAlgoritmo/src/DecimoSexto.java
@@ -6,59 +6,46 @@ public class DecimoSexto {
 
         int alunos;
         String nome;
+        String alunom = "";
+        String alunop = "";
         float nota;
+        float maior = 0f;
+        float menor = Float.MAX_VALUE;
+        int repet;
 
         System.out.println("-----------------------------------");
         System.out.println("    ESCOLA DOMINGOS ALBUQUERQUE    ");
         System.out.println("-----------------------------------");
 
-        System.out.println("Quantos alunos a turma tem? ");
+        System.out.print("Quantos alunos a turma tem? ");
         alunos = input.nextInt();
 
         System.out.println("-----------------------------------");
-        for(){
 
-            System.out.println("ALUNO: " +alunos);
+        for (repet = 1; repet <= alunos; repet++) {
+            System.out.println("ALUNO " + repet);
+            System.out.print("Nome do aluno: ");
+            nome = input.next();
+            System.out.print("Nota de " + nome + ":");
+            nota = input.nextFloat();
+            System.out.println("------------------");
 
-            nome  = input.next();
-            System.out.println("Nome do aluno: " +nome);
-            
+            if (nota > maior) {
+                maior = nota;
+                alunom = nome;
+            }
+            if (nota < menor) {
 
-
+                menor = nota;
+                alunop = nome;
+            }
 
         }
-
-
-
-
-
-
-
-
-
-
-
-        //falta termimar
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+        System.out.println("O melhor aproveitamento foi de " + alunom + " com a nota " + maior);
+        System.out.println("A menor nota foi de " + alunop + " com a nota " + menor);
 
     }
 
 }
+
+// tive ajuda do chatgpt
