5142177a5f07c3d5e4a882fbca4c12b3b4c68f60
Monge Naoki <tgtallles362@gmail.com>
2023-04-14 17:10:51 -0300

    novo tipo de repeticao em fase de aprendizagem

---
+++ cursoAlgoritmo/src/DecimoSetimo.java
@@ -0,0 +1,53 @@
+import java.util.Scanner;
+
+public class DecimoSetimo {
+    public static void main(String[] args) {
+     Scanner input = new Scanner(System.in);
+        int n;
+        int s;
+        String resp;
+
+        resp = "s";
+        while (resp = "s"; true); {
+            System.out.println("Digite o valor: ");
+            n = input.nextInt();
+            s = s+n;
+            System.out.println("Você quer continuar? [s/n]");
+            resp = input.next();
+
+        }
+        System.out.println("A soma de todos os valores é: " +s);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    }
+}
