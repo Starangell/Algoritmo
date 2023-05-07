b21e532899a8404336ce2fb57b7249fcdafe6bca
Monge Naoki <tgtallles362@gmail.com>
2023-05-07 16:30:19 -0300

    conclusão de mais uma aula,novo codigo e iamagens

--- cursoAlgoritmo/src/AnalisadorStrings.java
+++ cursoAlgoritmo/src/AnalisadorStrings.java
@@ -5,24 +5,21 @@ public class AnalisadorStrings {
         Scanner input = new Scanner(System.in);
 
         String n;
-        int posicao;
-        int codigo;
-        int c;
 
         System.out.print("Digite seu nome: ");
         n = input.next();
         System.out.println("Total de letras do seu nome: " + n.length());
-        System.out.println("Seu nome em maiusculas é " + n.toUpperCase());
-        System.out.println("Seu nome em maiusculas é " + n.toLowerCase());
-        System.out.println("A primeira letra do seu nome é " + n.substring(0, 1));
-        System.out.println("A ultima letra do seu nome é " + n.substring(8, 9));
-        posicao = n.indexOf('v');
-        System.out.println("Seu nome tem a letra V na posição " + posicao);
-        codigo = n.charAt((posicao));
-        System.out.println("O codigo da letra é " + codigo);
-        System.out.println("A letra de codigo 118 é " + Character.toString(118));
+        System.out.println("Seu nome em maisculas é " + n.toUpperCase());
+        System.out.println("Seu nome em minusculas é " + n.toLowerCase());
+        System.out.println("A primeira letra do seu nome é " + n.charAt(0));
+        System.out.println("A ultima letra do seu nome é " + n.toUpperCase().charAt(n.length() - 1));
+        System.out.println("Seu nome tem a letra A na posição " + n.toUpperCase().indexOf("A"));
+        System.out.println("O codigo da letra " + " 'a' " + " é " + Character.codePointAt((n), 4));
+        System.out.println("A letra de codigo 97 é: " + Character.toString(97));
+
+        for (int c = n.length() - 1; c >= 0; c--) {
+            System.out.print(n.charAt(c));
+        }
 
     }
 }
-// obs: depois melhoror o codigo(para fazer umas correções etc);
-// obs: terminar de fazer esse codigo
---
+++ cursoAlgoritmo/src/TesteVetores.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+
+public class TesteVetores {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+
+        //em fase de aprendizagem aula 14
+
+    }
+
+}
