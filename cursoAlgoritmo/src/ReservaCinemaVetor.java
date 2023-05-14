f309d8a2ebcdd06f4631e02421b4b5bc1e46fe2e
Monge Naoki <tgtallles362@gmail.com>
2023-05-14 17:20:35 -0300

    alteração no codigo/commir diario

--- cursoAlgoritmo/src/ReservaCinemaVetor.java
+++ cursoAlgoritmo/src/ReservaCinemaVetor.java
@@ -3,22 +3,28 @@ import java.util.Scanner;
 public class ReservaCinemaVetor {
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
-        String reserva[] = new String[10];
-        String sn;
+        //String reserva[] = new String[10];
+        String sn = "";
+        String reserva = "";
 
+        
+
+     do {
         System.out.println("[ B1 ][ B2 ][ B3 ][ B4 ][ B5 ][ B6 ][ B7 ][ B8 ][ B9 ][ B10 ]");
         System.out.println("-------------------------------------------------------------");
+        System.out.print("Reservar a Cadeira: " + "B");
+            reserva = input.next();
 
-        while (sn.equalsIgnoreCase("N")) {
+            System.out.println("Deseja Continuar? [S/N] ");
+            sn = input.next();
+        
+     } while (sn.equalsIgnoreCase("S"));
+
             
-        for(){ 
-        System.out.println("Reservar a Cadeira: "+"B");
-        reserva[] = input.next();
- }
-       
-    }
+      
     }
 
 }
 // exercicio aula 14 (Reseva cadeira cinema)
 // Em fase de construção
+//lembrete: fazer armanezar no array
