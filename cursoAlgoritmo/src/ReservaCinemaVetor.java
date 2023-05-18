b289c1fb22272c50f38fcf7508e2ee73fa901edf
Monge Naoki <tgtallles362@gmail.com>
2023-05-18 17:28:27 -0300

    commit diario,alterações no codigo tentando terminar

--- cursoAlgoritmo/src/ReservaCinemaVetor.java
+++ cursoAlgoritmo/src/ReservaCinemaVetor.java
@@ -3,30 +3,29 @@ import java.util.Scanner;
 public class ReservaCinemaVetor {
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
-        String reserva[] = {" [ B1 ][ B2 ][ B3 ][ B4 ][ B5 ][ B6 ][ B7 ][ B8 ][ B9 ][ B10 ] "};
+        String reserva[] = new String[10];
         String sn = "";
-       
 
-        
+        System.out.println("[ B1 ][ B2 ][ B3 ][ B4 ][ B5 ][ B6 ][ B7 ][ B8 ][ B9 ][ B10 ]");
+        System.out.println("-------------------------------------------------------------");
+
         do {
-            System.out.println("[ B1 ][ B2 ][ B3 ][ B4 ][ B5 ][ B6 ][ B7 ][ B8 ][ B9 ][ B10 ]");
-            System.out.println("-------------------------------------------------------------");
-            System.out.print("Reservar a Cadeira: " + "B");
-                reserva = input.next();
-    
-                
-    
-                System.out.println("Deseja Continuar? [S/N] ");
-                sn = input.next();
-            
-         } while (sn.equalsIgnoreCase("S"));
-            
-      
+            for (int i = 1; i <= 1; i++) {
+                System.out.print("Reservar a Cadeira: " + "B");
+                reserva[i] = input.next();
+
+            }
+
+            System.out.print("Deseja Continuar? [S/N] ");
+            sn = input.next();
+        } while (sn.equalsIgnoreCase("S"));
+
     }
 
 }
 // exercicio aula 14 (Reseva cadeira cinema)
 // Em fase de construção
-//lembrete: testar codigo
-//commit diario 2
-//cleverson ta me dando uma pequena ajuda para fazer os exercicios dessa aula(opinando dando ideais e etc);
+// lembrete: testar codigo
+// commit diario 3
+// cleverson ta me dando uma pequena ajuda para fazer os exercicios dessa
+// aula(opinando dando ideais e etc);
