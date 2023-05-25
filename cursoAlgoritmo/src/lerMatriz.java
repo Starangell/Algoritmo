d4d97cae45897a931fb7c577fead42d71af9835f
MN <tgtallles362@gmail.com>
2023-05-25 17:47:05 -0300

    commit diario, voltando aos poucos, novo codigo

---
+++ cursoAlgoritmo/src/lerMatriz.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+
+public class lerMatriz {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+
+        int[][] mat = new int[3][2];
+
+        int l;
+        int c;
+
+        for (l = 0; l < 3; l++) {
+            for (c = 0; c < 2; c++) {
+                System.out.println("Digite o valor da posição " + " [ " + (l+1)+ " , " + (c+1) + " ] ");
+                mat[l][c] = input.nextInt();
+
+            }
+        }
+
+    }
+
+}
+// Pratica 1: (aula 15 matriz)
+//to meio desligado e tive ajuda do chatgpt para corrigir umas coisas
+//codigo feito no linux
+//codigo incompleto
