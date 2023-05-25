f4dae8b3bc72351b7b42b4c9a0c5dcff74c33be4
MN <107052006+Starangell@users.noreply.github.com>
2023-05-25 10:01:11 -0300

    commit

--- cursoAlgoritmo/src/quartoExercicio.java
+++ cursoAlgoritmo/src/quartoExercicio.java
@@ -1,6 +1,6 @@
 import java.util.Scanner;
 
-public class QuartoExercicio {
+public class quartoExercicio {
     public static void main(String[] args) {
 
         Scanner input = new Scanner(System.in);
