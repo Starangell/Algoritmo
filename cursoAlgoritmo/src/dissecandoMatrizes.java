1d42a0eae5f9daf42f1dc4f10fca9894a67362bf
MN <107052006+Starangell@users.noreply.github.com>
2023-05-31 19:27:34 -0300

    alteração de codigo,Provavelmente terminação da saga de commit diario

--- cursoAlgoritmo/src/dissecandoMatrizes.java
+++ cursoAlgoritmo/src/dissecandoMatrizes.java
@@ -9,11 +9,11 @@ public class dissecandoMatrizes {
         int l = 0;
         int s = 0;
         int c = 0;
-
+        int t = 0;
         for (l = 0; l < 4; l++) {
             for (c = 0; c < 4; c++) {
                 System.out.print("Digite o valor para a posição " + "[" + (l + 1) + "," + (c + 1) + "]");
-                matriz[l][c] = input.nextInt();
+               // matriz[l][c] = input.nextInt();
             }
         }
         do {
@@ -38,31 +38,13 @@ public class dissecandoMatrizes {
                     }
                     break;
                 case 2:
-                for (l = 0; l < 1; l++) {
-                    for (c = 0; c < 1; c++) {
-                        System.out.print(matriz[l][c] + "  ");
+                    for (l = 0; l < 4; l++) {
+                        System.out.print(matriz[l][l] + "  ");
+                        for (t = 0; t < 0; t++) {
+                            System.out.println();
+                        }
                     }
-                    System.out.println();
-                }
-                for (l = 1; l < 2; l++) {
-                    for (c = 1; c < 2; c++) {
-                        System.out.print(matriz[l][c] + "  ");
-                    }
-                    System.out.println();
-                }
-                for (l = 2; l < 3; l++) {
-                    for (c = 2; c < 3; c++) {
-                        System.out.print(matriz[l][c] + "  ");
-                    }
-                    System.out.println();
-                }
-                for (l = 3; l < 4; l++) {
-                    for (c = 3; c < 4; c++) {
-                        System.out.print(matriz[l][c] + "  ");
-                    }
-                    System.out.println();
-                }
-                    
+                    //peguei essa resposta no git do curso em video
 
                     break;
 
